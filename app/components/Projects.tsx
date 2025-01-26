b62289a3025c7a707e7706775735da546edf619c
Akshit Gupta <akshu.24gupta@gmail.com>
2025-01-26 15:41:02 +0530

    scrollbar styling fix

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,9 +1,57 @@
+import { ProjectItems } from "@/lib/constants";
+import Image from "next/image";
+
 const Projects = () => {
   return (
-    <div id="Projects">
-      <h1 className="text-sm ">Projects</h1>
+    <div id="Projects" className="flex flex-col gap-8 w-full">
+      <h1 className="text-base ">Projects</h1>
+      <div className="flex flex-col gap-10 w-full">
+        {ProjectItems.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-row justify-start items-start gap-20 w-full"
+          >
+            <p className="text-xs text-gray-400 ">{item.date}</p>
+            <div className="flex flex-col gap-1 ">
+              <div className=" flex flex-row items-center gap-4 w-full ">
+                <p className="text-sm">{item.name}</p>
+
+                <a
+                  className="text-blue-500 underline underline-offset-2 hover:text-blue-400 duration-200 text-xs"
+                  href={item.url}
+                >
+                  live-url
+                </a>
+                <a
+                  className="text-blue-500 underline underline-offset-2 hover:text-blue-400 duration-200 text-xs"
+                  href={item.github}
+                >
+                  code
+                </a>
+              </div>
+              <p className="text-sm text-gray-500 ">{item.description}</p>
+              <ImageGallery imagesUrl={item.images} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Projects;
+
+const ImageGallery = ({ imagesUrl }: { imagesUrl: string[] }) => {
+  return (
+    <div className="w-full flex flex-row gap-4 overflow-x-scroll  py-4 max-w-[400px] scrollbar-thin scrollbar-thumb-gray-200 scrollbar-track-gray-100 scrollbar-visible">
+      {imagesUrl.map((url, index) => (
+        <div
+          key={index}
+          className="flex-shrink-0 h-full flex flex-col justify-center items-center"
+        >
+          <img src={url} className="w-[139px] rounded-lg" alt="ProjectImage" />
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ lib/constants.ts
@@ -0,0 +1,37 @@
+export const ProjectItems = [
+  {
+    name: "Craftix",
+    date: "Dec 2024",
+    description:
+      "AI react code generator and compiler. Free for unlimited usage.",
+    url: "https://craftix.akshit.app/",
+    github: "https://github.com/Ak2407/craftix",
+    images: [
+      "https://res.cloudinary.com/read-cv/image/upload/c_limit,h_2048,w_2048/v1/1/profileItems/Vd7asZ9ExjNlTgrXHoyyFxsprA52/m54pZVKRqybanrRHQl1o/4a88b1ae-f8f7-460c-b1a2-cd8fe0f63223.png?_a=DATAdtAAZAA0",
+      "https://res.cloudinary.com/read-cv/image/upload/c_limit,h_2048,w_2048/v1/1/profileItems/Vd7asZ9ExjNlTgrXHoyyFxsprA52/m54pZVKRqybanrRHQl1o/ae013924-c027-43e8-b909-148e143b98c8.png?_a=DATAdtAAZAA0",
+      "https://res.cloudinary.com/read-cv/image/upload/c_limit,h_2048,w_2048/v1/1/profileItems/Vd7asZ9ExjNlTgrXHoyyFxsprA52/m54pZVKRqybanrRHQl1o/e3f0ccc2-90d1-4362-aa67-2f5550ed053e.png?_a=DATAdtAAZAA0",
+      "https://res.cloudinary.com/read-cv/image/upload/c_limit,h_2048,w_2048/v1/1/profileItems/Vd7asZ9ExjNlTgrXHoyyFxsprA52/m54pZVKRqybanrRHQl1o/2642966f-1bd6-4e3e-a4cf-f4b589835e6e.png?_a=DATAdtAAZAA0",
+    ],
+  },
+  // {
+  //   name: "Imagis",
+  //   date: "Oct 2024",
+  //   description: "AI image generator. Free for unlimited usage.",
+  //   url: "https://imagis.akshit.app/",
+  //   github: "https://github.com/Ak2407/imagis",
+  // },
+  // {
+  //   name: "Notes",
+  //   date: "Oct 2024",
+  //   description: "Apple Notes AI clone. Ask questions visually by drawing.",
+  //   url: "https://notes.akshit.app/",
+  //   github: "https://github.com/Ak2407/notes",
+  // },
+  // {
+  //   name: "V0.1",
+  //   date: "Sep 2024",
+  //   description: "AI chatbot made using server actions and session management.",
+  //   url: "https://v0-1.akshit.app/",
+  //   github: "https://github.com/Ak2407/v0.1",
+  // },
+];
